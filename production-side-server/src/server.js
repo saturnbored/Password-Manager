768d85e3298aab916fa665bfb8a387f3cd72b0f7
Lakshya Gupta <lakshyag9775@gmail.com>
2022-02-19 23:05:52 +0530

    add TLS on production server

--- production-side-server/src/server.js
+++ production-side-server/src/server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const postroute = require("../routes/routes");
 const queryFuncs = require("../db/queryFunc");
+const path = require('path');
+const fs = require('fs');
+const https = require("https");
 
 
 //creating a server
@@ -11,15 +14,23 @@ const app = express();
 //middleware
 //Only if user got authenticated.
 app.use('/userdata', postroute);
+app.use('/userdata', (req,res)=>res.send("hlw from https server"));
 
 
 //listening to the server.
 const port = process.env.PORT || 8080;
 
+const secureServer = https.createServer({
+    key : fs.readFileSync(path.join(__dirname ,'..', 'TLS' , 'private.pem')),
+    cert : fs.readFileSync(path.join(__dirname ,'..', 'TLS' , 'cert.pem'))
+},app);
 
-app.listen(port, ()=>{
+
+secureServer.listen(port, ()=>{
     console.log(`Server is running on port : ${port}`);
+    
 });
+
 
 
 
