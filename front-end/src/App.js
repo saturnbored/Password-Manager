8f358b310a59481ab41cd9c59c9cdcff260954cd
Lakshya Gupta <lakshyag9775@gmail.com>
2022-02-19 22:54:23 +0530

    add login signup pages for frontend

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import LoginForm from './pages/LoginForm';
+import Signup from './pages/Signup';
+import {BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <Routes>
+          <Route exact path ='/' element={<LoginForm />} />
+          <Route exact path ='/register' element={<Signup />} />
+      </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
